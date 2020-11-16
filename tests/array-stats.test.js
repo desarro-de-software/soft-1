731177f0c74599af498895a4962204b9887aca5a
Maxi <maxifiorito77@gmail.com>
2020-11-16 00:39:25 -0300

    Delta Testing

--- tests/array-stats.test.js
+++ tests/array-stats.test.js
@@ -68,4 +68,30 @@ describe('tests for ArrayStats class', () => {
     expect(mediann).toBe(3);
   });
 
+  test('test average empty', () => {
+    const average = arrayStats.average([]);
+    expect(average).toBeUndefined;
+  });
+
+  test('test average string', () => {
+    const average = arrayStats.average(["17","no se que onda",""]);
+    expect(average).toBe(NaN);
+  });
+
+  test('test max', () => {
+    const max = arrayStats.max([5,8,10]);
+    expect(max).toBe(10);
+  });
+
+  test('test max empty', () => {
+    const max = arrayStats.max([]);
+    expect(max).toBeUndefined;
+  });
+
+  test('test max string', () => {
+    const max = arrayStats.max(["17","no se que onda",""]);
+    expect(max).toBe(NaN);
+  });
+
+
 });
