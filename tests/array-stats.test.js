7b57db54c200bef27b34930c7a924673e47c6a88
unknown <63322342+gonzaveron@users.noreply.github.com>
2020-11-17 19:10:02 -0300

    "otra vez"

--- tests/array-stats.test.js
+++ tests/array-stats.test.js
@@ -27,5 +27,19 @@ describe('tests for ArrayStats class', () => {
     const max = arrayStats.max([]);
     expect(max).toBeUndefined;
   });
+  
+  test('test min happy case', () => {
+    const min1 = arrayStats.min([6,232,256,3,12,26,24,1234,7,98]);
+    expect(min1).toBe(3);
+  });
 
+  test('test min empty array', () => {
+    const min2 = arrayStats.min([]);
+    expect(min2).toBeUndefined;
+  });
+
+  test('test min string array', () => {
+    const min2 = arrayStats.min(["a","0","12","string","23","-1","bla"]);
+    expect(min2).toBe(NaN)
+  });
 });
