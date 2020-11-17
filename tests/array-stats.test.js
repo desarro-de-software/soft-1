ebd7c1c5030fc100846b78d9c32ebb9f596420e1
Maxi <maxifiorito77@gmail.com>
2020-11-17 20:01:11 -0300

    arreglando mi error

--- tests/array-stats.test.js
+++ tests/array-stats.test.js
@@ -12,7 +12,6 @@ describe('tests for ArrayStats class', () => {
     const average = arrayStats.average([1,2,2,3,1,2,2,1,3,3]);
     expect(average).toBe(2);
   });
-<<<<<<< HEAD
 
   test('test min happy case', () => {
     const min1 = arrayStats.min([6,232,256,3,12,26,24,1234,7,98]);
@@ -78,8 +77,6 @@ describe('tests for ArrayStats class', () => {
     const average = arrayStats.average(["17","no se que onda",""]);
     expect(average).toBe(NaN);
   });
-=======
->>>>>>> fe61771 (removiendo test avg string)
   
   test('test average empty', () => {
     const average = arrayStats.average([]);
@@ -89,11 +86,6 @@ describe('tests for ArrayStats class', () => {
   test('test max', () => {
     const max = arrayStats.max([5,8,10,2,4,1,7]);
     expect(max).toBe(10);
-  });
-
-  test('test max string', () => {
-    const max = arrayStats.max(["17","no se que onda",""]);
-    expect(max).toBe(NaN);
   });
 
   test('test max empty', () => {
