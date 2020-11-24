b0c2ed4bc4b72eeab676b6b79dde76d7a0b23bfc
Juan Pablo <juanpabloalvira@gmail.com>
2020-11-24 19:55:04 -0300

    agregando_test_median

--- tests/array-stats.test.js
+++ tests/array-stats.test.js
@@ -48,4 +48,24 @@ describe('tests for ArrayStats class', () => {
     expect(min2).toBeUndefined;
   });
 
+  test('test median best', () => {
+    const mediann = arrayStats.median([1,2,2,3,1,2,2,1,3,3,]);
+    expect(mediann).toBe(2);
+  });
+
+  test('test median empty', () => {
+    const mediann = arrayStats.median([]);
+    expect(mediann).toBeUndefined;
+  });
+/*
+  test('test median string', () => {
+    const mediann = arrayStats.median(['aaaaa','12', ,'impar','esto es un string','stronk']);
+    expect(mediann).toBe("12");
+  });
+*/
+  test('test median string and numbers', () => {
+    const mediann = arrayStats.median(['aaaaa',2,'12',4]);
+    expect(mediann).toBe(3);
+  });
+
 });
